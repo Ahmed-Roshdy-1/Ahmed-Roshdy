17a398d65583249e4382b6a8c233b08f6030da0a
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-08-16 02:51:31 +0200

    push

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-let spinerLoader = document.getElementById("loader");
+let spinerLoader = document.getElementById("loader"); // for Loader
+let list = document.querySelectorAll(".list"); // for Navigation
 
+// add active class in selected tab Navigation
+function activeLink() {
+  list.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
+}
+
+// Event Listener  ------------
+
+// set active class for Navigation
+list.forEach((item) => item.addEventListener("click", activeLink));
+
+// Loader for window
 window.addEventListener("load", function () {
   spinerLoader.hidden = true;
 });
